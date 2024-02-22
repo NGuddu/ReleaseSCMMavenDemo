049d5cc3198bfbe2b17ce3bcd20f0e120d6285b9
Naiyyar <alishapn21@gmail.com>
2024-02-22 12:51:05 +0530

    third update

--- src/main/java/com/naiyyar/HelloWorld.java
+++ src/main/java/com/naiyyar/HelloWorld.java
@@ -6,6 +6,10 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		ArrayList list = new ArrayList<>();	
+		list.add("Hello");
+		list.add("Hello");
+		list.add("Hello");
+		list.add("Hello");
 		System.out.println("Hello");
 
 	}
